e8bdd15743bab80c903b5ad3d5db036e7a7c8cb0
Gael Rubin <grubin@c1r2s1.42lausanne.ch>
2021-10-20 14:01:22 +0200

    youpi

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,59 @@
 /*   By: grubin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 10:36:37 by grubin            #+#    #+#             */
-/*   Updated: 2021/10/20 10:38:12 by grubin           ###   ########.fr       */
+/*   Updated: 2021/10/20 14:00:52 by grubin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+static size_t	count_strs(char const *s, char c)
+{
+	size_t	count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			count++;
+			while (*s != '\0' && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t		len;
+	size_t		index;
+	const char	*start;
+	char		**split;
+
+	split = (char **) malloc(((count_strs(s, c)) + 1) * sizeof(*split));
+	if (!split)
+		return (0);
+	index = 0;
+	while (*s != '\0')
+	{
+		while (*s && *s == c)
+			s++;
+		start = s;
+		len = 0;
+		while (*s && *s != c)
+		{
+			s++;
+			len++;
+		}
+		if (*(s - 1) != c)
+			split[index++] = ft_substr(start, 0, len);
+	}
+	split[index] = 0;
+	return (split);
+}
+/*
 int	count_word(const char *s, char c)
 {
 	int	i;
@@ -31,17 +79,9 @@ int	count_word(const char *s, char c)
 		i++;
 		}
 	return (count);
-}
-	
-
-
-
-/*char	**ft_split(char const *s, char c)
-{
-	if (!s1 || !c)
-		return (NULL);
 }*/
-
+	
+/*
 int main()
 {
 	char const *s = " coucou les loulous ";
@@ -51,4 +91,4 @@ int main()
 	return (0);
 }
 
-
+*/
